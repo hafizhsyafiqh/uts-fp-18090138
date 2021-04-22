93d6409792430bacab968544c91ff459bd26d2bc
hafizhsyafiqh <hafizhsyafiqh@gmail.com>
2021-04-22 21:50:20 +0700

    first commit

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -5,26 +5,26 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
-        <title>18090138</title>
+        <title>UTS 6B D4 Teknik Informatika</title>
     </head>
     <body>
         <div class="container">
             <div class="card mt-5">
                 <div class="card-header bg-danger text-center">
-                    CRUD Data Kendaraan - <a href="#" target="_blank"> Hafizh Umar Syafiqh</a>
+                    CRUD Data Pegawai <a href="#" target="_blank"></a>
                 </div>
             </div>
 
                 <div class="card-body">
-                    <a href="/pegawai/tambah" class="btn btn-dark">Input Surat</a>                    <a href="/" class="btn btn-primary">Kembali</a>
+                    <a href="/pegawai/tambah" class="btn btn-dark">Input Pegawai</a>                    <a href="/" class="btn btn-primary">Kembali</a>
                     <br/>
                     <br/>
                     <table class="table table-bordered table-hover table-striped">
                         <thead>
                             <tr>
-                                <th>Tanggal Surat</th>
-                                <th>Judul Surat</th>
-                                <th>Action</th>
+                                <th>Nama</th>
+                                <th>Alamat</th>
+                                <th>OPSI</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -45,7 +45,7 @@
         </div>
          <!-- footer section start -->
     <footer>
-        <span>Created By <a href="#">Hafizh Umar Syafiqh</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+        <span>Created By <a href="#">Getar Jagat Dewantara</a> | <span class="far fa-copyright"></span> 2020 All rights reserved.</span>
     </footer>
 
     <script src="script.js"></script>
